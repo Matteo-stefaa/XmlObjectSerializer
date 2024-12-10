8139f060c104720e4e9f5bd979d92772ec08a55c
Matteo Stefani <matteo.stefani0101@gmail.com>
2024-12-10 09:27:26 +0100

    ss

---
+++ XmlObjectSerializer/Attributes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlObjectSerializer
+{
+    public class SerNodeAttribute : Attribute
+    {
+        public string Name { get; set; }
+    }
+    public class SerIenumerableNodeAttribute : SerNodeAttribute
+    {
+        public Type Type { get; set; }
+    }
+}
---
+++ XmlObjectSerializer/test/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlObjectSerializer.test
+{
+    public class Class1
+    {
+        [SerNode("Name")]
+        public string Name { get; set; }
+        [SerNode("Description")]
+        public string Description { get; set; }
+        [SerIenumerableNode("List", typeof(string))]
+        public List<string> List { get; set; } = new List<string>();
+    }
+}
